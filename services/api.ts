323c32aa50b8702a98a0692c78a2548fdcf1a024
quynnhopee <quynnhopee@hopee.com.vn>
2025-06-23 10:09:45 +0700

    update

--- services/api.ts
+++ services/api.ts
@@ -14,7 +14,7 @@ const api = axios.create({
 api.interceptors.request.use(
   (config) => {
     if (import.meta.client) {
-      const token = localStorage.getItem("auth_token");
+        const token = localStorage.getItem("auth_token");
       if (token) {
         config.headers.Authorization = `Bearer ${token}`;
       }
